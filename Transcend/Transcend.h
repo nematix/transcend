46e1c9461749e82804b3289ce3289048aa210cd8
Azri Jamil <azri@nematix.com>
2014-11-15 12:16:12 +0800

    Initial Commit

---
+++ Transcend/Transcend.h
@@ -0,0 +1,19 @@
+//
+//  Transcend.h
+//  Transcend
+//
+//  Created by Azri Jamil on 11/15/14.
+//  Copyright (c) 2014 Nematix System. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Transcend.
+FOUNDATION_EXPORT double TranscendVersionNumber;
+
+//! Project version string for Transcend.
+FOUNDATION_EXPORT const unsigned char TranscendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transcend/PublicHeader.h>
+
+
